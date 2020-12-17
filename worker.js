152692e2dd9217cd4b5920bc207ff28854beb148
XhmikosR <xhmikosr@gmail.com>
2020-12-17 18:19:34 +0200

    worker: switch to a class?

--- worker.js
+++ worker.js
@@ -1,20 +1,19 @@
 'use strict'
 
 const { EventEmitter } = require('events')
-const { inherits } = require('util')
 
-function Worker (data) {
-  EventEmitter.call(this)
+class Worker extends EventEmitter {
+  constructor (data) {
+    super(data)
 
-  if (typeof data === 'object' && !Array.isArray(data)) {
-    const self = this
+    if (typeof data === 'object' && !Array.isArray(data)) {
+      const self = this
 
-    Object.keys(data).forEach(k => {
-      self[k] = data[k]
-    })
+      Object.keys(data).forEach(k => {
+        self[k] = data[k]
+      })
+    }
   }
 }
-
-inherits(Worker, EventEmitter)
 
 module.exports = Worker
